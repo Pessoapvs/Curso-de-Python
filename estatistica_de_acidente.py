5a131b9029d59d2847ddc91e67cf1d3881300859
Person Vieira <142951418+Pessoapvs@users.noreply.github.com>
2025-11-19 18:10:49 -0300

    correção01

---
+++ estatistica_de_acidente.py
@@ -0,0 +1,16 @@
+print('Estátistica de Acidente de Trabalho')
+#LEGENDA: NA - Número de Acidentes:
+#LEGENDA: HT - Horas Trabalhadas:
+#LEGENDA: DP - Dias Perdidos:
+#LEGENDA: TF - Taxa de Frequência (TF):
+#LEGENDA: TG - Taxa de Gravidade (TG):   
+
+mes=input('Digite os mês de  referência; ')
+
+na=float(input('Digite a quantidade de acidentes:  '))
+dp=float(input('Digite a quantidade de dias perdidos: '))
+ht=float(input('Digite a  quantidade de horas trabalhadas: '))
+tf=(na*1.000000)/ht
+tg=(dp*1.000000)/ht
+print('A taxa de Gravidade de Acidente do més {} é {} '.format(mes, tg))
+print('A taxa de  frequência de Acidentes de traabalho do mês é:{} '.format(tf,))
---
+++ parede.py
@@ -0,0 +1,6 @@
+largura=float(input('Digite  a largura da parede: '))
+altura=float(input('digite a alturada parede:'))
+d=largura*altura
+l=d/2
+print('Sua parede tem a dimenção de {} x {} e a sua área e de {}m'.format(largura, altura,d))
+print('Para pintar a parede voçe vai prescisar de {} litros de tinta.'.format(l))
